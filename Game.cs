2af55f0f14769702cb33f4180a7725a9ecf9f57a
sean.gannon <sean.gannon@cgi.com>
2019-12-13 10:23:19 -0600

    fix syntax errors

--- Game.cs
+++ Game.cs
@@ -2,43 +2,16 @@
 using System.Collections.Generic;
 
 
+/**
 
-public class Game
-{
-
-static void Setup()
-{
-	Console.WriteLine("Please place your ships");
-
-
-
-}
-
-
-
-
-    static void Main()
-    {
-
-		Game.Welcome();
-
-		
-
-
-
-
-
-		GameLoop();
-		DisplayWinner();
-		Restart();
-        /*  const int GRID_SIZE = 10;
+		const int GRID_SIZE = 10;
 		List<Ship> defaultShipList = new List<Ship>();
 		defaultShipList.Add(new Ship("Battleship", 'B', 3));
 		List<Player> players = new List<Player>();
-        
-        Gameboard testG = new Gameboard();
+				
+				Gameboard testG = new Gameboard();
 
-        //set up players
+				//set up players
 		Player player1 = new Player(0, "Player1", GRID_SIZE);
 		Player player2 = new Player(1, "player2", GRID_SIZE);
 		
@@ -52,5 +25,49 @@ static void Setup()
 				//maybe get from the console the location of the ship and add it to the player's board
 			}
 		}
-*/ }
+	*/ 
+ 
+public class Game
+{
+
+	static void Setup()
+	{
+		Console.WriteLine("Please place your ships");
+
+
+
+	}
+
+
+
+
+	static void Main()
+	{
+
+		Game.Welcome();
+		Game.GameLoop();
+		Game.DisplayWinner();
+		Game.Restart();
+	}
+  private static void Welcome()
+  {
+    throw new NotImplementedException();
+  }
+ 
+  private static void GameLoop()
+  {
+    throw new NotImplementedException();
+  }
+
+  private static void DisplayWinner()
+  {
+    throw new NotImplementedException();
+  }
+
+ 	private static void Restart()
+  {
+    throw new NotImplementedException();
+  }
+
+
 }
--- View.cs
+++ View.cs
@@ -8,9 +8,8 @@ public class View
     {
         Console.WriteLine("Welcome to battleship!!!");
         View.PauseForUserInput("Press any key to continue");
-        
-
     }
+
     static void PauseForUserInput(string prompt)
     {
         Console.WriteLine(prompt);
@@ -23,15 +22,15 @@ public class View
     {
         Console.WriteLine("Player 1 press any key");
         View.PauseForUserInput("You are Player 1");
-        Player player1 = new Player(1, "player1", 10, 1);
+        // Player player1 = new Player(1, "player1", 10, 1);
         Console.WriteLine("Player 2 press any key");
         View.PauseForUserInput("You are Player 2");
-        Player player2 = new Player(2, "player2", 10, 2);
-        bool playerCount = true;
-        if(numberOfPlayers != 2)
-        {
+        // Player player2 = new Player(2, "player2", 10, 2);
+        // bool playerCount = true;
+        // if(numberOfPlayers != 2)
+        // {
             
-        }
+        // }
     }
 
     /* static void validatePlayers()
